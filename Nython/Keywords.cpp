22bdf60065357a711c9b123c49a3ee2ed89b63e0
Nicholas Baron <njbaron@cpp.edu>
2018-08-21 13:01:32 -0700

    Changed Keywords to a namespace

--- Nython/Keywords.cpp
+++ Nython/Keywords.cpp
@@ -3,99 +3,144 @@
 #include <algorithm>
 #include <cctype>
 
-std::vector<std::string>	Keywords::operators = {"=", "==", "<", "<=", ">", ">=", "++", "--", "->", "-", "+", "+=", "*", "**", "/", "%"};
-std::vector<std::string>	Keywords::delineators = {"(",")","|"};
-std::vector<std::string>	Keywords::commands = {"print","repeat","loop","ret","return", "endline", "if", "else", "elif", "endif"};
-std::vector<std::string>	Keywords::types = {"int","void","float","string","char", "bool"};
+namespace Keywords {
+	namespace {
+		std::vector<std::string>	operators = {"=", "==", "<", "<=", ">", ">=", "++", "--", "->", "-", "+", "+=", "*", "**", "/", "%"};
+		std::vector<std::string>	delineators = {"(",")","|"};
+		std::vector<std::string>	commands = {"print","repeat","loop","ret","return", "endline", "if", "else", "elif", "endif"};
+		std::vector<std::string>	types = {"int","void","float","string","char", "bool"};
+	}
 
-bool Keywords::getBoolFromToken(const Token* value) {
-	std::string lowered(value->text);
-	std::transform(value->text.begin(), value->text.end(), lowered.begin(), std::tolower);
+	bool isDelineator(const std::string& item) { return VectorUtil::contains(delineators, item); }
+	bool isOperator(const std::string& item) { return VectorUtil::contains(operators, item); }
+	bool isCommand(const std::string& item) { return VectorUtil::contains(commands, item); }
 
-	if(lowered == "true" || lowered == "1") {
+	bool isBinaryOp(const Token* tok) { return isOperator(tok->text) && tok->text != "++" && tok->text != "--" && tok->text != "!"; }
+	bool isBoolOp(const Token* tok) {
+		return isOperator(tok->text) && (tok->text[0] == '<' || tok->text[0] == '>' || tok->text[0] == '!' || tok->text == "==");
+	}
+
+	bool getBoolFromToken(const Token* value) {
+		std::string lowered(value->text);
+		std::transform(value->text.begin(), value->text.end(), lowered.begin(), std::tolower);
+
+		if(lowered == "true" || lowered == "1") {
+			return true;
+		} else if(lowered == "false" || lowered == "0") {
+			return false;
+		}
+		std::cout << "The value of " << value->text << " is recognized as true." << std::endl;
 		return true;
-	} else if(lowered == "false" || lowered == "0") {
-		return false;
-	}
-	std::cout << "The value of " << value->text << " is recognized as true." << std::endl;
-	return true;
-}
-
-VariableType Keywords::bestFit(const Token* tok) {
-	if(tok->text.find_first_of(' ') != std::string::npos) {
-		return VariableType::STRING;
-	} else if(tok->text == "true" || tok->text == "false") {
-		return VariableType::BOOL;
-	} else if(tok->text.length() == 3 && tok->text[0] == tok->text[2]) {
-		return VariableType::CHAR;
 	}
 
-	bool canBeInt = true, canBeFloat = true, previousDot = false;
-	for(unsigned i = 0; i < tok->text.length(); i++) {
-		if(!isdigit(tok->text[i])) {
-			canBeInt = false;
-			if(tok->text[i] == '.') {
-				if(previousDot) {
-					canBeFloat = false;
+	VariableType bestFit(const Token* tok) {
+		if(tok->text.find_first_of(' ') != std::string::npos) {
+			return VariableType::STRING;
+		} else if(tok->text == "true" || tok->text == "false") {
+			return VariableType::BOOL;
+		} else if(tok->text.length() == 3 && tok->text[0] == tok->text[2]) {
+			return VariableType::CHAR;
+		}
+
+		bool canBeInt = true, canBeFloat = true, previousDot = false;
+		for(unsigned i = 0; i < tok->text.length(); i++) {
+			if(!isdigit(tok->text[i])) {
+				canBeInt = false;
+				if(tok->text[i] == '.') {
+					if(previousDot) {
+						canBeFloat = false;
+					}
+					previousDot = true;
 				}
-				previousDot = true;
 			}
+		}
+
+		if(canBeInt) {
+			return VariableType::INT;
+		} else if(canBeFloat) {
+			return VariableType::FLOAT;
+		}
+
+		return VariableType::STRING;
+	}
+
+	TokenType getTokenType(const std::string & text) {
+		const auto first_space = text.find_first_of(" ");
+		if(first_space != std::string::npos) {
+			return TokenType::LITERAL;
+		}
+
+		TokenType type = TokenType::IDENTIFIER;
+
+		if(VectorUtil::contains(operators, text)) {
+			type = TokenType::OPERATOR;
+		} else if(VectorUtil::contains(delineators, text)) {
+			type = TokenType::DELINEATOR;
+		} else if(VectorUtil::contains(commands, text)) {
+			type = TokenType::COMMAND;
+		} else if(VectorUtil::contains(types, text)) {
+			type = TokenType::TYPE;
+		} else if(text.find_first_of('\"') == 0 || text.find_first_of('\'') == 0 || isdigit(text[0])) {
+			type = TokenType::LITERAL;
+		}
+
+		return type;
+	}
+
+	VariableType getVarType(const Token & tok) {
+		if(tok.type != TokenType::TYPE) {
+			return VariableType::VOID;
+		} else if(tok.text == "int") {
+			return VariableType::INT;
+		} else if(tok.text == "string") {
+			return VariableType::STRING;
+		} else if(tok.text == "float") {
+			return VariableType::FLOAT;
+		} else if(tok.text == "char") {
+			return VariableType::CHAR;
+		} else {
+			return VariableType::VOID;
 		}
 	}
 
-	if(canBeInt) {
-		return VariableType::INT;
-	} else if(canBeFloat) {
-		return VariableType::FLOAT;
+	void printKeywords() {
+		std::cout << "Operators: " << operators << std::endl;
+		std::cout << "Delineators: " << delineators << std::endl;
+		std::cout << "Commands: " << commands << std::endl;
+		std::cout << "Variable Types: " << types << std::endl;
 	}
 
-	return VariableType::STRING;
-}
+	int opMath(const std::string & op, int left, int right) {
+		if(op == "+") {
+			return left + right;
+		} else if(op == "-") {
+			return left - right;
+		} else if(op == "*") {
+			return left * right;
+		} else if(op == "/") {
+			return left / right;
+		} else if(op == "%") {
+			return left % right;
+		}
 
-TokenType Keywords::getTokenType(const std::string & text) {
-	const auto first_space = text.find_first_of(" ");
-	if(first_space != std::string::npos) {
-		return TokenType::LITERAL;
+		std::cerr << "[ERR] Unimplemented int math operator: " << op << std::endl;
+		return 0;
 	}
 
-	TokenType type = TokenType::IDENTIFIER;
+	float opMath(const std::string & op, float left, float right) {
+		if(op == "+") {
+			return left + right;
+		} else if(op == "-") {
+			return left - right;
+		} else if(op == "*") {
+			return left * right;
+		} else if(op == "/") {
+			return left / right;
+		}
 
-	if(VectorUtil::contains(operators, text)) {
-		type = TokenType::OPERATOR;
-	} else if(VectorUtil::contains(delineators, text)) {
-		type = TokenType::DELINEATOR;
-	} else if(VectorUtil::contains(commands, text)) {
-		type = TokenType::COMMAND;
-	} else if(VectorUtil::contains(types, text)) {
-		type = TokenType::TYPE;
-	} else if(text.find_first_of('\"') == 0 || text.find_first_of('\'') == 0 || isdigit(text[0])) {
-		type = TokenType::LITERAL;
+		std::cerr << "[ERR] Unimplemented float math operator: " << op << std::endl;
+		return 0;
 	}
-
-	return type;
-}
-
-VariableType Keywords::getVarType(const Token & tok) {
-	if(tok.type != TokenType::TYPE) {
-		return VariableType::VOID;
-	} else if(tok.text == "int") {
-		return VariableType::INT;
-	} else if(tok.text == "string") {
-		return VariableType::STRING;
-	} else if(tok.text == "float") {
-		return VariableType::FLOAT;
-	} else if(tok.text == "char") {
-		return VariableType::CHAR;
-	} else {
-		return VariableType::VOID;
-	}
-}
-
-void Keywords::printKeywords() {
-	std::cout << "Operators: " << operators << std::endl;
-	std::cout << "Delineators: " << delineators << std::endl;
-	std::cout << "Commands: " << commands << std::endl;
-	std::cout << "Variable Types: " << types << std::endl;
 }
 
 std::ostream & operator<<(std::ostream & lhs, const TokenType & rhs) {
--- Nython/Keywords.h
+++ Nython/Keywords.h
@@ -21,42 +21,37 @@ struct Token {
 std::ostream& operator<<(std::ostream& lhs, const Token& rhs);
 bool operator==(const Token& lhs, const Token& rhs);
 
-class Keywords {
-private:
-	static std::vector<std::string> operators, delineators, commands, types;
+namespace Keywords {
+	bool isDelineator(const std::string& item);
+	bool isOperator(const std::string& item);
+	bool isCommand(const std::string& item);
 
-public:
-	inline static bool isDelineator(const std::string& item) { return VectorUtil::contains(delineators, item); }
-	inline static bool isOperator(const std::string& item) { return VectorUtil::contains(operators, item); }
-	inline static bool isCommand(const std::string& item) { return VectorUtil::contains(commands, item); }
+	inline bool isFirstOfPairDelin(const Token* tok) { return tok->type == TokenType::DELINEATOR && tok->text == "("; }
+	inline bool isSecondOfPairDelin(const Token* tok) { return tok->type == TokenType::DELINEATOR && tok->text == ")"; }
+	inline bool nonPairedDelin(const Token* tok) { return tok->type == TokenType::DELINEATOR && !isFirstOfPairDelin(tok) && !isSecondOfPairDelin(tok); }
 
-	inline static bool isFirstOfPairDelin(const Token* tok) { return tok->type == TokenType::DELINEATOR && tok->text == "("; }
-	inline static bool isSecondOfPairDelin(const Token* tok) { return tok->type == TokenType::DELINEATOR && tok->text == ")"; }
-	inline static bool nonPairedDelin(const Token* tok) { return tok->type == TokenType::DELINEATOR && !isFirstOfPairDelin(tok) && !isSecondOfPairDelin(tok); }
+	bool isBinaryOp(const Token* tok);
+	bool isBoolOp(const Token* tok);
 
-	inline static bool isBinaryOp(const Token* tok) { return isOperator(tok->text) && tok->text != "++" && tok->text != "--" && tok->text != "!"; }
-	inline static bool isBoolOp(const Token* tok) {
-		return isOperator(tok->text) && (tok->text[0] == '<' || tok->text[0] == '>' || tok->text[0] == '!' || tok->text == "==");
-	}
-	inline static bool isAssignment(const Token* tok) { return tok->type == TokenType::OPERATOR && isBinaryOp(tok) && (tok->text == "=" || tok->text == "+="); }
-	inline static bool isAssignAfterOp(const Token* tok) { return isAssignment(tok) && tok->text[0] != '='; }
-	inline static bool isUnaryOp(const Token* tok) { return !isBinaryOp(tok) && isOperator(tok->text); }
-	inline static bool canUseIncre(const VariableType& type) { return type == VariableType::FLOAT || type == VariableType::INT; }
+	inline bool isAssignment(const Token* tok) { return tok->type == TokenType::OPERATOR && isBinaryOp(tok) && (tok->text == "=" || tok->text == "+="); }
+	inline bool isAssignAfterOp(const Token* tok) { return isAssignment(tok) && tok->text[0] != '='; }
+	inline bool isUnaryOp(const Token* tok) { return !isBinaryOp(tok) && isOperator(tok->text); }
+	inline bool canUseIncre(const VariableType& type) { return type == VariableType::FLOAT || type == VariableType::INT; }
 
-	inline static bool isLoopStart(const Token* tok) { return tok->type == TokenType::COMMAND && tok->text == "repeat"; }
-	inline static bool isConditionalStart(const Token* tok) { return tok->type == TokenType::COMMAND && (tok->text == "if" || tok->text == "elif"); }
-	inline static bool isEndOfLoop(const Token* tok) { return tok->type == TokenType::COMMAND && tok->text == "loop"; }
-	inline static bool isEndOfConditional(const Token* tok) { return tok->type == TokenType::COMMAND && (tok->text == "elif" || tok->text == "else" || tok->text == "endif"); }
-	inline static bool needsParameter(const Token* tok) {
+	inline bool isLoopStart(const Token* tok) { return tok->type == TokenType::COMMAND && tok->text == "repeat"; }
+	inline bool isConditionalStart(const Token* tok) { return tok->type == TokenType::COMMAND && (tok->text == "if" || tok->text == "elif"); }
+	inline bool isEndOfLoop(const Token* tok) { return tok->type == TokenType::COMMAND && tok->text == "loop"; }
+	inline bool isEndOfConditional(const Token* tok) { return tok->type == TokenType::COMMAND && (tok->text == "elif" || tok->text == "else" || tok->text == "endif"); }
+	inline bool needsParameter(const Token* tok) {
 		return tok->type == TokenType::COMMAND && (tok->text == "print" || tok->text == "if" || tok->text == "elif" || tok->text == "else");
 	}
 
-	static bool getBoolFromToken(const Token* tok);
+	bool getBoolFromToken(const Token* tok);
 
-	inline static bool isFuncEnd(const Token* tok) { return tok->type == TokenType::COMMAND && (tok->text == "ret" || tok->text == "return"); }
+	inline bool isFuncEnd(const Token* tok) { return tok->type == TokenType::COMMAND && (tok->text == "ret" || tok->text == "return"); }
 	//Returns the optimal type for a variable
-	static VariableType bestFit(const Token* tok);
-	inline static bool compatibleVarTypes(const VariableType origin, const VariableType dest) {
+	VariableType bestFit(const Token* tok);
+	inline bool compatibleVarTypes(const VariableType origin, const VariableType dest) {
 		if(origin == dest) {
 			return true;
 		} else if(origin == VariableType::VOID || dest == VariableType::VOID) {
@@ -68,81 +63,43 @@ public:
 		return false;
 	}
 
-	static TokenType getTokenType(const std::string& text);
-	static VariableType getVarType(const Token& tok);
+	TokenType getTokenType(const std::string& text);
+	VariableType getVarType(const Token& tok);
 
-	static void printKeywords();
+	void printKeywords();
 
 	template<class T, class U>
-	static bool opTest(const std::string& op, T left, U right);
+	inline bool opTest(const std::string& op, T left, U right) {
+		if(op == "<") {
+			return left < right;
+		} else if(op == "==") {
+			return left == right;
+		} else if(op == ">") {
+			return left > right;
+		} else if(op == ">=") {
+			return left >= right;
+		} else if(op == "<=") {
+			return left <= right;
+		}
 
-	static int opMath(const std::string& op, int left, int right);
-	static float opMath(const std::string& op, float left, float right);
+		std::cerr << "[ERR] Unimplemented test operator: " << op << std::endl;
+		return false;
+	}
+
+	int opMath(const std::string& op, int left, int right);
+	float opMath(const std::string& op, float left, float right);
 
 	template<class T>
-	static void opUnary(const std::string& op, T& var);
-};
-
-template<class T, class U>
-inline bool Keywords::opTest(const std::string & op, T left, U right) {
-	if(op == "<") {
-		return left < right;
-	} else if(op == "==") {
-		return left == right;
-	} else if(op == ">") {
-		return left > right;
-	} else if(op == ">=") {
-		return left >= right;
-	} else if(op == "<=") {
-		return left <= right;
-	}
-
-	std::cerr << "[ERR] Unimplemented test operator: " << op << std::endl;
-	return false;
-}
-
-inline int Keywords::opMath(const std::string & op, int left, int right) {
-	if(op == "+") {
-		return left + right;
-	} else if(op == "-") {
-		return left - right;
-	} else if(op == "*") {
-		return left * right;
-	} else if(op == "/") {
-		return left / right;
-	} else if(op == "%") {
-		return left % right;
-	}
-
-	std::cerr << "[ERR] Unimplemented int math operator: " << op << std::endl;
-	return 0;
-}
-
-inline float Keywords::opMath(const std::string & op, float left, float right) {
-	if(op == "+") {
-		return left + right;
-	} else if(op == "-") {
-		return left - right;
-	} else if(op == "*") {
-		return left * right;
-	} else if(op == "/") {
-		return left / right;
-	}
-
-	std::cerr << "[ERR] Unimplemented float math operator: " << op << std::endl;
-	return 0;
-}
-
-template<class T>
-inline void Keywords::opUnary(const std::string & op, T& var) {
-	if(op == "++") {
-		var++;
-	} else if(op == "--") {
-		var--;
-	} else if(op == "!") {
-		var = !var;
-	} else {
-		std::cerr << "[ERR] Unimplemented unary operator: " << op << std::endl;
+	inline void opUnary(const std::string& op, T& var) {
+		if(op == "++") {
+			var++;
+		} else if(op == "--") {
+			var--;
+		} else if(op == "!") {
+			var = !var;
+		} else {
+			std::cerr << "[ERR] Unimplemented unary operator: " << op << std::endl;
+		}
 	}
 }
 
@@ -154,5 +111,4 @@ inline void Keywords::opUnary(const std::string & op, bool& var) {
 		std::cerr << "[ERR] Unimplemented bool unary operator: " << op << std::endl;
 	}
 }
-
 #endif // !_KEYWORDS
